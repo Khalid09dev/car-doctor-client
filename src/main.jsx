55964b41514b53340d92e799a668fae84fe1f500
khalid09dev <mdkhalidhossen@hotmail.com>
2024-05-02 21:29:52 +0600

    added a authProvider and authentication system with firebase

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import {
   RouterProvider
 } from 'react-router-dom';
 import router from './Routes/Routes.jsx'
+import AuthProvider from './providers/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='max-w-7xl mx-auto'>
       <React.StrictMode>
-        <RouterProvider router={router}></RouterProvider>
+        <AuthProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </AuthProvider>
       </React.StrictMode>
   </div>
 )
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,17 +2,19 @@ import { useContext } from 'react';
 import {Link} from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
 
-const Login = () => {
-    const {signIn} = useContext(AuthContext);
+
+const SignUp = () => {
+    const {createUser} = useContext(AuthContext);
 
     const handleLogin = (e) => {
         e.preventDefault();
         const form = e.target;
+        const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
+        console.log(name, email, password)
 
-        signIn(email, password)
+        createUser(email, password)
         .then((result) => {
             console.log(result.user)
         })
@@ -21,16 +23,18 @@ const Login = () => {
         })
     }
     return (
-        <div className="hero min-h-screen bg-slate-100 py-10 flex gap-16 justify-center">
+        <div className="hero min-h-screen bg-slate-100 flex gap-16 py-20 justify-center">
             <img src="https://i.postimg.cc/kG2CGq4Q/login.png" alt="userLogin" />
             <div className="bg-white rounded-lg">
             <form onSubmit={handleLogin} className="px-16 pt-16 pb-0">
-                <h3 className="text-[#444] pb-5 text-center text-[40px] font-semibold">Login</h3>
+                <h3 className="text-[#444] pb-5 text-center text-[40px] font-semibold">Sign Up</h3>
+                <label htmlFor="" className="text-[#444] text-[16px] font-semibold">Name</label> <br />
+                <input type="text" name="name" id="" className="bg-white py-2.5 placeholder:text-[14px] font-normal pr-[110px] mb-4 border-[1px] rounded-lg pl-6" placeholder="Your Name"/> <br />
                 <label htmlFor="" className="text-[#444] text-[16px] font-semibold">Email</label> <br />
                 <input type="email" name="email" id="" className="bg-white py-2.5 placeholder:text-[14px] font-normal pr-[110px] mb-4 border-[1px] rounded-lg pl-6" placeholder="Your Email"/> <br />
                 <label htmlFor="" className="text-[#444] text-[16px] font-semibold">Confirm Password</label> <br />
                 <input type="password" name="password" id="" className="bg-white py-2.5 placeholder:text-[14px] font-normal pr-[110px] mb-4 border-[1px] rounded-lg pl-6" placeholder="Your Password"/> <br />
-                <button className="text-center text-white py-3 rounded-lg w-full bg-[#FF3811] font-semibold">Sign In</button>
+                <button className="text-center text-white py-3 rounded-lg w-full bg-[#FF3811] font-semibold">Sign Up</button>
             </form>
             <p className="text-[#444] text-center py-4 text-base font-medium">Or Sign In with</p>
                 <div className="flex justify-center gap-3 items-center"> 
@@ -38,10 +42,10 @@ const Login = () => {
                     <img src="https://i.postimg.cc/KYMygL6R/bx-bxl-linkedin.png" alt="linkedin" className="bg-[#F5F5F8] p-2 rounded-full"/>
                     <img src="https://i.postimg.cc/HW3FnBr6/google-1.png" alt="google" className="bg-[#F5F5F8] p-2.5 rounded-full"/>
                 </div>
-                <p className="text-base font-normal text-center pb-16 pt-7 text-[#737373]">New to Car Doctor? <Link to="/signup" className="text-[#FF3811]">Sign Up</Link></p>
+                <p className="text-base font-normal text-center pb-16 pt-7 text-[#737373]">Already have an account? <Link to="/login" className="text-[#FF3811]">Login</Link></p>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
